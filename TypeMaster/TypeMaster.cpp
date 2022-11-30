438ea86c542ce5afcc8b227066ed787b937008db
Serhii <mywaifuqueen@gmail.com>
2022-11-30 13:51:15 +0000

    11/30/2022 1:51

--- TypeMaster/TypeMaster.cpp
+++ TypeMaster/TypeMaster.cpp
@@ -1,4 +1,4 @@
-#include <windows.h>
+﻿#include <windows.h>
 #include <windowsX.h>
 #include <tchar.h>
 #include <commctrl.h>
@@ -13,35 +13,39 @@
 
 
 
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
 {
 	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
 }
 
 
+HANDLE hMutex;
 
-BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp)
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	
-	srand(time(NULL));
 	switch (message)
 	{
 	case WM_CLOSE:
 		EndDialog(hWnd, 0);
 		return TRUE;
 
-	case  WM_INITDIALOG: {
-		SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)brush);
-
+	case WM_INITDIALOG: {
+		hMutex = CreateMutex(NULL, FALSE, TEXT("{D99CD3E0-670D-4def-9B74-99FD7E793DFB}"));
+		DWORD dwAnswer = WaitForSingleObject(hMutex, 0);
+		if (dwAnswer == WAIT_TIMEOUT)
+		{
+			MessageBox(hWnd, TEXT("You cannot run this application twice."), TEXT("ERROR"), MB_OK | MB_ICONERROR);
+			EndDialog(hWnd, 0);
+		}
 		return TRUE;
-	}
 
+	}
 	case WM_COMMAND: {
-
-
-		return TRUE;
-	}
+		
 	}
 
+	}
 	return FALSE;
 }
--- TypeMaster/resource.h
+++ TypeMaster/resource.h
@@ -13,6 +13,9 @@
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
 #define IDB_BITMAP1                     130
+#define IDI_ICON1                       131
+#define IDI_ICON2                       133
+#define IDD_DIALOG2                     134
 #define IDC_BUTTON1                     1004
 #define IDC_BUTTON2                     1005
 #define IDC_BUTTON3                     1006
@@ -51,9 +54,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1033
+#define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
